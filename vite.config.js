ba850e15049320670ab15b6ee0ffd8de7634a594
ilycode2@gmail.com <70672465+IlhamAbdul07@users.noreply.github.com>
2026-04-21 20:54:00 +0700

    soolving GH Pages2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/UserProfile-UstFauzan",
+  base: "/UserProfile-UstFauzan/",
   plugins: [react()],
 });
